7e6a078f5b23649b6326a5aa74a9b43be3352186
vuvk <panknd@mail.ru>
2021-02-08 14:04:59 +0500

    class for light sources

---
+++ core/src/com/vuvk/swinger/objects/LightSource.java
@@ -0,0 +1,95 @@
+/**
+    Copyright (C) 2021 Anton "Vuvk" Shcherbatykh <vuvk69@gmail.com>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package com.vuvk.swinger.objects;
+
+import java.awt.Color;
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ *
+ * @author Anton "Vuvk" Shcherbatykh
+ */
+public class LightSource extends Object3D implements Serializable {
+    transient public final static List<LightSource> LIB = new CopyOnWriteArrayList<>();
+
+    private Color color;
+    private double radius;
+
+    public LightSource() {
+        this(Color.WHITE);
+    }
+
+    public LightSource(Color color) {
+        this(color, 1.0);
+    }
+
+    public LightSource(Color color, double radius) {
+        setColor(color);
+        setRadius(radius);
+
+        LIB.add(this);
+    }
+
+    @Override
+    public void destroy() {
+        synchronized (LIB) {
+            LIB.remove(this);
+        }
+    }
+
+    @Override
+    public void finalize() {
+        destroy();
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    public static LightSource[] getLib() {
+        return LIB.toArray(new LightSource[LIB.size()]);
+    }
+
+    public static void updateAll() {
+        synchronized (LIB) {
+            LIB.forEach(LightSource::update);
+        }
+    }
+
+    public static void deleteAll() {
+        synchronized (LIB) {
+            LIB.clear();
+        }
+    }
+
+}
